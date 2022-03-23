f7cc25b1a1e561ef08d24b54640da9582d9b42e4
david.dichmont <dichmont@aol.com>
2022-03-23 22:46:58 +0000

    commit 2

---
+++ HotelBookings/BookingManager.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelBookings;
+public class BookingManager : IBookingManager
+{
+    public bool IsRoomAvailable(int room, DateTime date)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddBooking(string guest, int room, DateTime date)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ HotelBookings/IBookingManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HotelBookings
+{
+    public interface IBookingManager
+    {
+        private bool IsRoomAvailable(int room, DateTime date);
+
+        void AddBooking(string guest, int room, DateTime date);
+    }
+}
